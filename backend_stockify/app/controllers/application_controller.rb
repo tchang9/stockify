35a4d913d00d3935f2c2ef0cbc00a50f58d4222f
tchang9 <tlchang9@gmail.com>
2019-10-03 18:44:09 -0400

    removed print statements in backend

--- backend_stockify/app/controllers/application_controller.rb
+++ backend_stockify/app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::API
         begin
             stock_api_url = URI.parse("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker}&apikey=LEQZN4AR3VUUZHLU")
             stock_info = Net::HTTP.get_response(stock_api_url).body
-            puts JSON.parse(stock_info)
             if JSON.parse(stock_info)["Global Quote"]
                 return JSON.parse(stock_info)
             else
